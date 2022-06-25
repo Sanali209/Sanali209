15bfc9d6d3d3883bf6506e82ef67cdf0c91d0ec2
Sanali209 <sanali209.gmail.com>
2022-06-25 08:38:07 +0300

    Update index.php

--- pdflib/index.php
+++ pdflib/index.php
@@ -1,3 +1,31 @@
+<html>
+<head>
+<title>PDFlib Example: index</title>
+</head>
+<body>
+<h1>PDFlib Example: index</h1>
+<p>
+<i>index</i> is a simple test page for the indexing functions.
+</p>
+<p>
+<i>index</i> creates a PDF file with a table of contents.
+</p>
+<p>
+The table of contents is created by calling the <i>index</i> function.
+</p>
+<!-- create table viz columns name,image,description-->
+<table border="1">
+<tr>
+<td>name</td>
+<td>image</td>
+<td>description</td>
+</tr>
+
+<body>
+<html>
+//create html table forr show data from database
+// function create_table_html($table_name) {
+
 <?php
 
 //add required includes once
